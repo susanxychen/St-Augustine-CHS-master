0aad2fb0c1c34169e77dc5827092575b0a087b7f
Susan Chen <susanxy.chen@gmail.com>
2019-11-16 23:49:56 -0500

    Initial commit

---
+++ St Augustine CHS/FAQCell.swift
@@ -0,0 +1,16 @@
+//
+//  FAQCell.swift
+//  St Augustine CHS
+//
+//  Created by Susan Chen on -11-162019.
+//  Copyright © 2019 St Augustine CHS. All rights reserved.
+//
+
+import UIKit
+
+class FAQCell: UITableViewCell {
+
+    
+    }
+
+}
